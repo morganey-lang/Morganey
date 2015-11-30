28c0ace347939a64de2e00c29eb39b940f974b0c
rexim <reximkut@gmail.com>
2015-11-30 13:32:43 +0600

    Fix the compilation error
    `LambdaFunction` and `LambdaApplication` should extend LambdaTerm

--- src/main/scala/me/rexim/LambdaTerm.scala
+++ src/main/scala/me/rexim/LambdaTerm.scala
@@ -4,8 +4,8 @@ sealed trait LambdaTerm
 
 case class LambdaVariable(name: String) extends LambdaTerm
 
-case class LambdaFunction(parameter: LambdaVariable, body: LambdaTerm) {
+case class LambdaFunction(parameter: LambdaVariable, body: LambdaTerm) extends LambdaTerm {
   def apply(argument: LambdaTerm): LambdaTerm = ???
 }
 
-case class LambdaApplication(f: LambdaFunction, argument: LambdaTerm)
+case class LambdaApplication(f: LambdaFunction, argument: LambdaTerm) extends LambdaTerm
