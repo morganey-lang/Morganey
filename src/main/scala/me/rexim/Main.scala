690836a025754b15e55ad20b9dd0815c43f2b774
rexim <reximkut@gmail.com>
2015-12-02 01:38:42 +0600

    Remove unused function (#1)

--- src/main/scala/me/rexim/Main.scala
+++ src/main/scala/me/rexim/Main.scala
@@ -1,12 +1,5 @@
 package me.rexim
 
 object Main {
-
-  def betaReduction(expression: LambdaTerm) =
-    expression match {
-      case LambdaApplication(f, argument) => f(argument)
-      case _ => expression
-    }
-
   def main(args: Array[String]): Unit = ???
 }
