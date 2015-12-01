9464b2c26c8f767d293dec4f244c0ff4f3665e2f
rexim <reximkut@gmail.com>
2015-12-02 01:57:15 +0600

    Better substitution syntax

--- src/main/scala/me/rexim/BetaReduction.scala
+++ src/main/scala/me/rexim/BetaReduction.scala
@@ -1,21 +1,22 @@
 package me.rexim
 
 object BetaReduction {
-  def substitute(t: LambdaTerm, v: LambdaVar, r: LambdaTerm): LambdaTerm = {
-    t match {
+  def substitute(term: LambdaTerm, substitution: (LambdaVar, LambdaTerm)): LambdaTerm = {
+    val (v, r) = substitution
+    term match {
       case LambdaVar(name) if name == v.name => r
-      case LambdaVar(name) if name != v.name => t
+      case LambdaVar(name) if name != v.name => term
       case LambdaApp(leftTerm, rightTerm) =>
-        LambdaApp(substitute(leftTerm, v, r), substitute(rightTerm, v, r))
-      case LambdaFunc(LambdaVar(name), body) if name == v.name => t
+        LambdaApp(substitute(leftTerm, v -> r), substitute(rightTerm, v -> r))
+      case LambdaFunc(LambdaVar(name), body) if name == v.name => term
       case LambdaFunc(LambdaVar(name), body) if name != v.name =>
-        LambdaFunc(LambdaVar(name), substitute(body, v, r))
+        LambdaFunc(LambdaVar(name), substitute(body, v -> r))
     }
   }
 
   def reduction(r: LambdaTerm): LambdaTerm = {
     r match {
-      case LambdaApp(LambdaFunc(x, t), s) => substitute(t, x, s)
+      case LambdaApp(LambdaFunc(x, t), s) => substitute(t, x -> s)
       case _ => r
     }
   }
--- src/test/scala/me/rexim/BetaReductionSpec.scala
+++ src/test/scala/me/rexim/BetaReductionSpec.scala
@@ -7,34 +7,34 @@ class BetaReductionSpec extends FlatSpec with Matchers with TestVariables {
   "A variable" should "be substituted with term" in {
     val variable = x
     val term = LambdaFunc(y, y)
-    BetaReduction.substitute(variable, x, term) should be (term)
+    BetaReduction.substitute(variable, x -> term) should be (term)
   }
 
   it should "not be substituted with term if it's different" in {
     val variable = x
     val term = LambdaFunc(y, y)
-    BetaReduction.substitute(variable, z, term) should be (variable)
+    BetaReduction.substitute(variable, z -> term) should be (variable)
   }
 
   "An application" should "be substituted recursively" in {
     val inputTerm = LambdaApp(x, y)
     val expectedTerm = LambdaApp(z, y)
 
-    BetaReduction.substitute(inputTerm, x, z) should be (expectedTerm)
+    BetaReduction.substitute(inputTerm, x -> z) should be (expectedTerm)
   }
 
   "A function" should "avoid substitution of its variable" in {
     val inputTerm = LambdaFunc(x, x)
     val expectedTerm = inputTerm
 
-    BetaReduction.substitute(inputTerm, x, z) should be (expectedTerm)
+    BetaReduction.substitute(inputTerm, x -> z) should be (expectedTerm)
   }
 
   "A function" should "be substituted" in {
     val inputTerm = LambdaFunc(y, x)
     val expectedTerm = LambdaFunc(y, z)
 
-    BetaReduction.substitute(inputTerm, x, z) should be (expectedTerm)
+    BetaReduction.substitute(inputTerm, x -> z) should be (expectedTerm)
   }
 
   "An application" should "be beta-reduced" in {
