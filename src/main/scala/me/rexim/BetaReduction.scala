c24b7477bd75e77ec0361a5493d5b97645f51717
rexim <reximkut@gmail.com>
2015-12-02 02:12:22 +0600

    Collapse some match branches for substitute

--- src/main/scala/me/rexim/BetaReduction.scala
+++ src/main/scala/me/rexim/BetaReduction.scala
@@ -4,15 +4,18 @@ object BetaReduction {
   def substitute(term: LambdaTerm, substitution: (LambdaVar, LambdaTerm)): LambdaTerm = {
     val (v, r) = substitution
     term match {
-      case x: LambdaVar if x == v => r
-      case x: LambdaVar if x != v => term
+      case x: LambdaVar =>
+        if (x == v) r
+        else term
+
       case LambdaApp(leftTerm, rightTerm) =>
         LambdaApp(
           substitute(leftTerm, v -> r),
           substitute(rightTerm, v -> r))
-      case LambdaFunc(x, body) if x == v => term
-      case LambdaFunc(x, body) if x != v =>
-        LambdaFunc(x, substitute(body, v -> r))
+
+      case LambdaFunc(x, body) =>
+        if (x == v) term
+        else LambdaFunc(x, substitute(body, v -> r))
     }
   }
 
