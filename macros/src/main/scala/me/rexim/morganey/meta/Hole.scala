8384ff120e188ae64c5ff411f1ca491a9b6d51b2
keddelzz <keddelzz@web.de>
2016-06-30 19:03:11 +0200

    Implemented first version of interpolator

---
+++ macros/src/main/scala/me/rexim/morganey/meta/Hole.scala
@@ -0,0 +1,18 @@
+package me.rexim.morganey.meta
+
+import java.util.regex.Pattern
+
+private[meta] object Hole {
+
+  private val holePattern   = "$hole"
+  private val holeRegex     = "\\$hole([0-9]+)".r
+
+  def apply(n: Int): String =
+    s"$holePattern$n"
+
+  def unapply(x: String): Option[Int] =
+    Option(x) collect {
+      case holeRegex(n) => n.toInt
+    }
+
+}
--- macros/src/main/scala/me/rexim/morganey/meta/QuotationMacro.scala
+++ macros/src/main/scala/me/rexim/morganey/meta/QuotationMacro.scala
@@ -1,7 +1,8 @@
 package me.rexim.morganey.meta
 
 import scala.reflect.macros.whitebox.Context
-import scala.util.{ Try => MTry, Success => MSucc }
+import scala.util.{ Try => MTry }
+import me.rexim.morganey.ast._
 
 private[meta] class TestMacro(val c: Context) {
   import c.universe._
@@ -16,10 +17,47 @@ private[meta] class TestMacro(val c: Context) {
     (mCall orElse lCall).get
   }
 
-  private val (parts, args) = macroApplication()
+  private val (parts, args)   = macroApplication()
+
+  private def buildProgram(): String = {
+    val pi = parts.iterator
+    val ai = args.iterator
+    val bldr = new StringBuilder(pi.next())
+    var i = 0
+    while (ai.hasNext) {
+      val part = Hole(i)
+      ai.next // skip element
+      i += 1
+      bldr ++= part
+      bldr ++= pi.next()
+    }
+    bldr.toString
+  }
+
+  private def parse(program: String): LambdaTerm = {
+    val par = MetaParser
+    par.parseAll(par.term, program) match {
+      case par.Success(res, _)       => res
+      case par.NoSuccess(err, input) =>
+        val pos = input.pos; import pos._
+        val msg = s"Error at line $line column $column in interpolated string:\n$err"
+        c.abort(c.enclosingPosition, msg)
+    }
+  }
+
+  private implicit def liftAst[A <: LambdaTerm]: Liftable[A] = Liftable {
+    case LambdaVar(Hole(n))      =>
+      args(n)
+    case LambdaVar(name)         =>
+      q"_root_.me.rexim.morganey.ast.LambdaVar($name)"
+    case LambdaFunc(param, body) =>
+      q"_root_.me.rexim.morganey.ast.LambdaFunc($param, $body)"
+    case LambdaApp(left, right)  =>
+      q"_root_.me.rexim.morganey.ast.LambdaApp($left, $right)"
+  }
 
   def quote(args: Tree*): Tree =
-    q"""Predef.println("Hi, Macro")"""
+    liftAst(parse(buildProgram()))
 
 }
 
