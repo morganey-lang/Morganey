36cd6e26a0fdaf0614788d011fc1045a0bd4fdf4
keddelzz <keddelzz@web.de>
2016-07-05 21:30:12 +0200

    Add tests for parser
    - character-literals
    - string-literals
    - number-literals
    - simple applications
    - simple lambda functions

--- kernel/src/test/scala/me/rexim/morganey/ParserSpec.scala
+++ kernel/src/test/scala/me/rexim/morganey/ParserSpec.scala
@@ -13,4 +13,51 @@ class ParserSpec extends FlatSpec with Matchers with TestTerms {
     term.successful should be (true)
     term.get should be (LambdaApp(I(x), I(y)))
   }
+
+  private val parse = LambdaParser.parseAll(LambdaParser.term, _: String)
+
+  private val validPrograms = Seq(
+    // string-literals
+    """"Hello!"""",
+    """"\tFoo\nBar!\n"""",
+    "\"\\\"\"",
+    "\"\\\'\"",
+
+    // char-literals
+    """'a'""",
+    """'\n'""",
+    """'''""",
+    """'\''""",
+    """'\"'""",
+    """'"'""",
+
+    // number-literals
+    "0",
+    "6",
+    "42",
+
+    "(λa.b)", "(a b)",
+    "(λa.(a b))", "((a b) (a b))"
+  )
+
+  for (program <- validPrograms) {
+    "Morganeys parser" should s"parse the valid program <$program> without any error" in {
+      parse(program).successful should be (true)
+    }
+  }
+
+  private val invalidPrograms = Seq(
+    "\"Hello",
+    "'a",
+    "(a b",
+    "(a . b)",
+    "(\\\\a . b)"
+  )
+
+  for (program <- invalidPrograms) {
+    "Morganeys parser" should s"parse the invalid program <$program> with a parse-error" in {
+      parse(program).successful should be(false)
+    }
+  }
+
 }
