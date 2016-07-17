fb02d73d7876c0cb90fecdb16943fad6dc59eb19
keddelzz <keddelzz@web.de>
2016-07-17 09:01:40 +0200

    Fix loading of modules on windows
    - Use UTF-8 as the default charset for loading files.

--- kernel/src/main/scala/me/rexim/morganey/util/package.scala
+++ kernel/src/main/scala/me/rexim/morganey/util/package.scala
@@ -1,5 +1,10 @@
 package me.rexim.morganey
 
+import java.io.{FileInputStream, InputStreamReader, Reader}
+import java.nio.charset.StandardCharsets.UTF_8
+
+import scala.util.Try
+
 package object util {
 
   /**
@@ -26,4 +31,10 @@ package object util {
       }
       case c => c + unquoteString(s.tail)
     }
+
+  def reader(path: String): Try[Reader] = {
+    val inputStream = Try(new FileInputStream(path))
+    inputStream.map(new InputStreamReader(_, UTF_8))
+  }
+
 }
