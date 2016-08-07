6693d5ae75dc7f6c87ef19af06a1e82c2538e67d
rexim <reximkut@gmail.com>
2016-08-07 20:30:51 +0600

    Add Specs for MorganeyExecutor (#82)

---
+++ src/test/scala/me/rexim/morganey/interpreter/MorganeyExecutorSpecs.scala
@@ -0,0 +1,27 @@
+package me.rexim.morganey.interpreter
+
+import me.rexim.morganey.ast._
+import me.rexim.morganey.ast.LambdaTermHelpers._
+import me.rexim.morganey.interpreter.MorganeyExecutor
+import org.scalatest._
+
+import scala.util.Success
+
+class MorganeyExecutorSpecs extends FlatSpec with Matchers {
+  val programBindings = List(
+    MorganeyBinding(lvar("x"), lvar("x")),
+    MorganeyBinding(lvar("y"), lvar("y")),
+    MorganeyBinding(lvar("z"), lvar("z"))
+  )
+
+  val programEntry = MorganeyBinding(lvar("main"), lvar("main"))
+
+  val rawProgram = programEntry :: programBindings
+
+  val programInput = "khooy".toStream
+
+  "Executor" should "compile a correct program to a reducible term" in {
+    val expectedProgram = lapp(lvar("main"), LambdaInput(programInput)).addBindings(programBindings)
+    MorganeyExecutor.compileProgram(programInput)(rawProgram) should be (Success(expectedProgram))
+  }
+}
