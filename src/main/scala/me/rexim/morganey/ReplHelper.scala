82b4278685175ed01034427b8bbc9f5d3032bce6
rexim <reximkut@gmail.com>
2016-08-21 21:32:39 +0600

    Wrap stuff with quotes (#126)
    - chars with single quotes
    - string with double quotes

--- src/main/scala/me/rexim/morganey/ReplHelper.scala
+++ src/main/scala/me/rexim/morganey/ReplHelper.scala
@@ -12,10 +12,11 @@ object ReplHelper {
 
   def smartShowTerm(term: LambdaTerm): String = {
     decodeNumber(term) match {
-      case Some(number) if isPrintableChar(number) => s"char: ${number.toChar}"
+      case Some(number) if isPrintableChar(number) => s"char: '${number.toChar}'"
       case Some(number) => s"number: $number"
       case None => decodeListOfNumbers(term) match {
-        case Some(numbers) if numbers.forall(isPrintableChar) => s"string: ${numbers.map(_.toChar).mkString}"
+        case Some(numbers) if numbers.forall(isPrintableChar) =>
+          s"""string: \"${numbers.map(_.toChar).mkString}\""""
         case Some(numbers) => s"numbers: [${numbers.mkString(",")}]"
         case None => s"term: $term"
       }
