dffc12f9eb7392a60709d5b11ca6f529d6523315
keddelzz <keddelzz@web.de>
2016-08-26 18:18:44 +0200

    Rewrite tail-recursion with lazy application of decoders

--- src/main/scala/me/rexim/morganey/interpreter/TermOutputHelper.scala
+++ src/main/scala/me/rexim/morganey/interpreter/TermOutputHelper.scala
@@ -3,38 +3,31 @@ package me.rexim.morganey.interpreter
 import me.rexim.morganey.ast.LambdaTerm
 import me.rexim.morganey.meta._
 
-import scala.annotation.tailrec
-
 object TermOutputHelper {
 
-  def smartShowTerm(term: LambdaTerm, noPrefix: Boolean = false): String = {
-    @tailrec
-    def decode(decoders: List[Decoder[_]]): String = decoders match {
-      case hd :: tl => hd(term) match {
-        case Some(result) => if (noPrefix) result else s"${hd.prefix}: $result"
-        case None         => decode(tl)
-      }
-      case Nil =>
-        // `idDecoder` won't ever return `None`
-        val result = idDecoder(term).get
-        if (noPrefix) result else s"${idDecoder.prefix}: $result"
-    }
-    decode(decoders)
+  def smartShowTerm(term: LambdaTerm): String = {
+    val (prefix, value) = decode(term)
+    s"$prefix: $value"
   }
 
-  private val idDecoder = Decoder[LambdaTerm]("term", _.toString)
+  private def decode(term: LambdaTerm): (String, String) =
+    decoders
+      .map(_(term))
+      .find(_.isDefined)
+      .flatten
+      .getOrElse(("term", term.toString))
 
-  private val decoders: List[Decoder[_]] =
-    List(
-      Decoder[Char]                     ("char", c => s"'$c'"),
-      Decoder[Int]                      ("number", _.toString),
-      Decoder[String]                   ("string", s => s"""\"$s\""""),
-      Decoder[Seq[Int]]                 ("numbers", _.mkString("[", ",", "]")),
-      Decoder[Seq[LambdaTerm]]          ("elements", _.map(smartShowTerm(_, true)).mkString("[", ",", "]"))
+  private val decoders: Stream[Decoder[_]] =
+    Stream(
+      Decoder[Char]            ("char", c => s"'$c'"),
+      Decoder[Int]             ("number", _.toString),
+      Decoder[String]          ("string", s => s"""\"$s\""""),
+      Decoder[Seq[Int]]        ("numbers", _.mkString("[", ",", "]")),
+      Decoder[Seq[LambdaTerm]] ("elements", _.map(decode(_)._2).mkString("[", ",", "]"))
     )
 
   private case class Decoder[T](prefix: String, transform: T => String)(implicit unT: Unliftable[T]) {
-    def apply(term: LambdaTerm): Option[String] = unT.unapply(term).map(transform)
+    def apply(term: LambdaTerm): Option[(String, String)] = unT.unapply(term).map(transform).map((prefix, _))
   }
 
 }
