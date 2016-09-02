476e83528aecd52dafc94c81d1d6b0b4df942d2d
rexim <reximkut@gmail.com>
2016-09-03 00:05:46 +0600

    Use proper dependent bindings in executor (#136)

--- src/main/scala/me/rexim/morganey/interpreter/MorganeyExecutor.scala
+++ src/main/scala/me/rexim/morganey/interpreter/MorganeyExecutor.scala
@@ -1,13 +1,13 @@
 package me.rexim.morganey.interpreter
 
 import scala.util._
-
 import me.rexim.morganey.ast._
 import me.rexim.morganey.syntax.{LambdaParser, LambdaParserException}
 import me.rexim.morganey.module._
 import me.rexim.morganey.util._
-
 import java.io.Reader
+
+import me.rexim.morganey.ast.error.{BindingLoop, NonExistingBinding}
 
 object MorganeyExecutor {
 
@@ -36,8 +36,19 @@ object MorganeyExecutor {
   def compileProgram(input: Stream[Char])(rawProgram: List[MorganeyBinding]): Try[LambdaTerm] = {
     rawProgram.partition(_.variable.name == "main") match {
       case (List(MorganeyBinding(LambdaVar("main"), program)), bindings) =>
-        Success(LambdaApp(program, LambdaInput(input)).addBindings(bindings))
-      case _ => Failure(new IllegalArgumentException(""))
+        LambdaApp(program, LambdaInput(input)).addDependentBindings(bindings) match {
+          case Right(compiledProgram) => Success(compiledProgram)
+
+          case Left(NonExistingBinding(name)) =>
+            Failure(new IllegalArgumentException(s"Non-existing binding: $name!"))
+
+          case Left(BindingLoop(loop)) =>
+            Failure(new IllegalArgumentException(
+              s"""|Binding loop detected: ${loop.mkString(" -> ")}
+                  |Please use Y-combinator if you want recursion
+               """.stripMargin))
+        }
+      case _ => Failure(new IllegalArgumentException("Entry point not found!"))
     }
   }
 }
