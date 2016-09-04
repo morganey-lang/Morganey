b82cc1feb91998e61a2635d43a07077369e5cc86
rexim <reximkut@gmail.com>
2016-09-04 17:37:09 +0600

    Add UTs for dependent bindings (#136)

--- src/test/scala/me/rexim/morganey/BindingsSpecs.scala
+++ src/test/scala/me/rexim/morganey/BindingsSpecs.scala
@@ -2,7 +2,9 @@ package me.rexim.morganey
 
 import me.rexim.morganey.ast.LambdaTermHelpers._
 import me.rexim.morganey.ast.MorganeyBinding
+import me.rexim.morganey.ast.error.{BindingLoop, NonExistingBinding}
 import me.rexim.morganey.helpers.TestTerms
+import me.rexim.morganey.meta._
 import org.scalatest._
 
 class BindingsSpecs extends FlatSpec with Matchers with TestTerms {
@@ -22,5 +24,23 @@ class BindingsSpecs extends FlatSpec with Matchers with TestTerms {
           lfunc("khooy", inputExpression),
           nya)),
       I(y)))
+  }
+
+  "Dependent bindings" should "produce NonExistingBinding error on non-existing binding" in {
+    val bindings = List(
+      MorganeyBinding(m"x", m"\\x. x"),
+      MorganeyBinding(m"y", m"\\y. y")
+    )
+
+    m"a".addDependentBindings(bindings) should be (Left(NonExistingBinding("a")))
+  }
+
+  "Dependent bindings" should "produce BindingLoop error on a loop in bindings" in {
+    val bindings = List(
+      MorganeyBinding(m"a", m"b"),
+      MorganeyBinding(m"b", m"a")
+    )
+
+    m"a".addDependentBindings(bindings) should be (Left(BindingLoop(List("a", "b", "a"))))
   }
 }
