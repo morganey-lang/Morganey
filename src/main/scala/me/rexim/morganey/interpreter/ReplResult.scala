f1aab944fd1786f8b2cb7c925ab4b59a0d7b88df
rexim <reximkut@gmail.com>
2016-09-12 00:41:03 +0600

    Remove redundant ReplResult method (#138)

--- src/main/scala/me/rexim/morganey/interpreter/ReplResult.scala
+++ src/main/scala/me/rexim/morganey/interpreter/ReplResult.scala
@@ -2,6 +2,4 @@ package me.rexim.morganey.interpreter
 
 import me.rexim.morganey.ast.LambdaTerm
 
-case class ReplResult(context: ReplContext, result : Option[LambdaTerm] = None) {
-  def flatMap(f: (ReplContext) => ReplResult): ReplResult = f(context)
-}
+case class ReplResult(context: ReplContext, result : Option[LambdaTerm] = None)
