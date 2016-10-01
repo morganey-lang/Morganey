5b024d010dfc94a71fd5a54232d025f80e75495f
keddelzz <keddelzz@web.de>
2016-10-01 19:03:34 +0200

    Use hidden-args in function 'unwrapNumber' (#233)

--- kernel/src/main/scala/me/rexim/morganey/church/ChurchNumberConverter.scala
+++ kernel/src/main/scala/me/rexim/morganey/church/ChurchNumberConverter.scala
@@ -2,6 +2,7 @@ package me.rexim.morganey.church
 
 import me.rexim.morganey.ast.LambdaTermHelpers.lnested
 import me.rexim.morganey.ast._
+import hiddenargs._
 
 import scala.annotation.tailrec
 
@@ -9,15 +10,15 @@ object ChurchNumberConverter {
 
   private val printableChars = Set(32 to 176 :_*).map(_.toChar)
 
+  @hiddenargs
   @tailrec
-  private def unwrapNumber(f: String, x: String, number: LambdaTerm, acc: Int = 0): Option[Int] = {
+  private def unwrapNumber(f: String, x: String, number: LambdaTerm, @hidden acc: Int = 0): Option[Int] =
     number match {
       case LambdaVar(x1) if x == x1 => Some(acc)
       case LambdaApp(LambdaVar(f1), restNumber)
         if f1 == f => unwrapNumber(f, x, restNumber, acc + 1)
       case _ => None
     }
-  }
 
   @tailrec
   private def wrapNumber(number: Int, acc: LambdaTerm = LambdaVar("x")): LambdaTerm = {
