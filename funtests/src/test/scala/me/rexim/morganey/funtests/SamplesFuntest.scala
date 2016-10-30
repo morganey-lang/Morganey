dfca4937a84be3762839a06aec7ca359f1915670
rexim <reximkut@gmail.com>
2016-10-30 22:52:10 +0600

    Add echo sample to funtests (#281)

--- funtests/src/test/scala/me/rexim/morganey/funtests/SamplesFuntest.scala
+++ funtests/src/test/scala/me/rexim/morganey/funtests/SamplesFuntest.scala
@@ -1,5 +1,7 @@
 package me.rexim.morganey.funtests
 
+import java.io.ByteArrayInputStream
+
 import org.scalatest._
 
 class SamplesFuntest extends FlatSpec with Matchers with MorganeyProcess {
@@ -12,6 +14,14 @@ class SamplesFuntest extends FlatSpec with Matchers with MorganeyProcess {
   "Multiplication sample" should "print the result of 2 * 3" in {
     val expectedOutput = s"number: 6${System.lineSeparator()}"
     val actualOutput = morganey("./docs/samples/02-multiplication.mgn").!!
+    actualOutput should be (expectedOutput)
+  }
+
+  "Echo sample" should "print back the user's input" in {
+    val input = new ByteArrayInputStream("Hello".getBytes("UTF-8"))
+    val expectedOutput = s"""string: \"Hello\"${System.lineSeparator()}"""
+    val actualOutput = morganey("./docs/samples/03-echo.mgn") #< input !!
+
     actualOutput should be (expectedOutput)
   }
 }
