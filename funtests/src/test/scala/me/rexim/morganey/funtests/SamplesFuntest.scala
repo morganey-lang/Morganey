b89c463440dd465ab8f299eef68652657b119022
rexim <reximkut@gmail.com>
2016-10-30 21:27:48 +0600

    Add SamplesFuntest with HelloWorld sample (#281)

---
+++ funtests/src/test/scala/me/rexim/morganey/funtests/SamplesFuntest.scala
@@ -0,0 +1,11 @@
+package me.rexim.morganey.funtests
+
+import org.scalatest._
+
+class SamplesFuntest extends FlatSpec with Matchers with MorganeyProcess {
+  "Hello World sample" should "print Hello World message" in {
+    val expectedOutput = s"""string: \"Hello, World\"${System.lineSeparator()}"""
+    val actualOutput = morganey("./docs/samples/hello-world.mgn").!!
+    actualOutput should be (expectedOutput)
+  }
+}
