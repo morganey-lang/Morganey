405ac69305b2a7c75b72077b8a7a2bb7d78fafa5
rexim <reximkut@gmail.com>
2016-12-04 18:00:43 +0600

    Add specs for Morganey index (#305)

--- src/test/scala/me/rexim/morganey/module/ModuleFinderSpec.scala
+++ src/test/scala/me/rexim/morganey/module/ModuleFinderSpec.scala
@@ -1,10 +1,14 @@
 package me.rexim.morganey.module
 
+import org.mockito.Mockito._
 import org.scalatest._
 import org.scalatest.mockito.MockitoSugar
-import java.io.File
+
+
+import java.io._
+import java.util.Vector
 import java.net.URL
-import org.mockito.Mockito._
+import java.nio.charset.StandardCharsets
 
 class ModuleFinderSpec extends FlatSpec with MockitoSugar with Matchers {
   val moduleFinder =
@@ -30,5 +34,31 @@ class ModuleFinderSpec extends FlatSpec with MockitoSugar with Matchers {
 
     moduleFinder.findModuleInClasspath("std.prelude") should
       be (Some(expectedUrl))
+  }
+
+  def mockMorganeyIndexUrl(modulesInIndex: List[String]): URL = {
+    val morganeyIndexContent = modulesInIndex.mkString("\n")
+
+    val morganeyIndexUrl = mock[URL]
+    when(morganeyIndexUrl.openStream())
+      .thenReturn(new ByteArrayInputStream(morganeyIndexContent.getBytes(StandardCharsets.UTF_8)))
+
+    morganeyIndexUrl
+  }
+
+  it should "return all modules from Morganey index" in {
+    import scala.collection.JavaConverters._
+
+    val moduleContainers = List(List("hello.mgn", "world.mgn"), List("a/foo.mgn", "b/bar.mgn"))
+    val expectedModules = moduleContainers.flatMap(identity).toSet
+    val mockedMorganeyIndexUrls = moduleContainers.map(mockMorganeyIndexUrl)
+
+    val classLoader = mock[ClassLoader]
+    when(classLoader.getResources("morganey-index"))
+      .thenReturn(new Vector(mockedMorganeyIndexUrls.asJava).elements())
+
+    val moduleFinder = new ModuleFinder(Nil, classLoader)
+
+    moduleFinder.findAllModulesInIndex() should be (expectedModules)
   }
 }
