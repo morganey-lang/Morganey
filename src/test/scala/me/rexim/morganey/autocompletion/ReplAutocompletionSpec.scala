9943a05bcfcfcbe3ecad9b77b1022dc98577be8b
rexim <reximkut@gmail.com>
2016-12-25 21:19:06 +0600

    Introduce ReplAutocompletionSpec with UTs (#316)
    AutocompletionSpec is reserved for integration testing

--- src/test/scala/me/rexim/morganey/autocompletion/ReplAutocompletionSpec.scala
+++ src/test/scala/me/rexim/morganey/autocompletion/ReplAutocompletionSpec.scala
@@ -2,7 +2,7 @@ package me.rexim.morganey.autocompletion
 
 import org.scalatest._
 
-class LastNameInLineSpec extends FlatSpec with Matchers {
+class ReplAutocompletionSpec extends FlatSpec with Matchers {
   "lastNameInLine" should "extract identifier suffix if it is present" in {
     ReplAutocompletion.lastNameInLine("hello") should be (Some((0, "hello")))
     ReplAutocompletion.lastNameInLine("#$%^ hello") should be (Some(5, "hello"))
