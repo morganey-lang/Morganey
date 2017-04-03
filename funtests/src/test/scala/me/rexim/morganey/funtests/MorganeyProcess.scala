1f54e65c6476fc6822c6e5cb0529bcc36c40f460
rexim <reximkut@gmail.com>
2017-04-03 22:53:39 +0700

    Add TODO for UX of morganey program execution (#330)

--- funtests/src/test/scala/me/rexim/morganey/funtests/MorganeyProcess.scala
+++ funtests/src/test/scala/me/rexim/morganey/funtests/MorganeyProcess.scala
@@ -5,5 +5,9 @@ import me.rexim.morganey.BuildInfo
 
 trait MorganeyProcess {
   def morganey(args: String*): ProcessBuilder =
+    // TODO(00beaa2c-945e-4df0-ad4f-3c38be47273c): improve usability of morganey program execution
+    //
+    // Specifying the full classpath and full entry classname just to
+    // run a program is not really usable for the user perspective
     s"java -cp .:./target/scala-2.11/morganey-assembly-${BuildInfo.version}.jar me.rexim.morganey.Main ${args.mkString(" ")}"
 }
